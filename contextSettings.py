acd2a2ffa2bfd55bf0ba0eee9e9ad4c673c892e6
Kingpin <l22bitt1@tuta.io>
2022-06-11 07:58:48 -0400

    Context

---
+++ contextSettings.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import xbmc
+
+if __name__ == '__main__':
+	plugin = 'plugin://plugin.video.kingpin/'
+	path = 'RunPlugin(%s?action=tools_contextKSSettings&opensettings=false)' % plugin
+	xbmc.executebuiltin(path)
+	# xbmc.executebuiltin('RunPlugin(%s?action=widgetRefresh)' % plugin) #now part of "tools_contextKSSettings" action
