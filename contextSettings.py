4ae0ccc91a37801644db160c9c790702ea1933b5
Kingpin <l22bitt1@tuta.io>
2022-06-11 23:33:55 -0400

    bumps = context script change

--- contextSettings.py
+++ contextSettings.py
@@ -4,6 +4,6 @@ import xbmc
 
 if __name__ == '__main__':
 	plugin = 'plugin://plugin.video.kingpin/'
-	path = 'RunPlugin(%s?action=tools_contextKSSettings&opensettings=false)' % plugin
+	path = 'RunPlugin(%s?action=tools_contextkingpinSettings&opensettings=false)' % plugin
 	xbmc.executebuiltin(path)
-	# xbmc.executebuiltin('RunPlugin(%s?action=widgetRefresh)' % plugin) #now part of "tools_contextKSSettings" action
+	# xbmc.executebuiltin('RunPlugin(%s?action=widgetRefresh)' % plugin) #now part of "tools_contextkingpinSettings" action
